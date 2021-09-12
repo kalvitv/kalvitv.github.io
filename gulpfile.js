cf60e2056336289d191f3a5928c5c240cf4122c1
exoplanet <kalvitv@yahoo.com>
2021-09-13 02:51:08 +0530

    Change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
 exports.default = function() {
-  return src('/assets/js/*.js')    
+  return src('assets/js/*.js')    
     .pipe(concat('kalvi-app.js'))
     .pipe(uglify())
-    .pipe(dest('/assets/js/kalvi-app.js'));
+    .pipe(dest('assets/js/'));
 }
