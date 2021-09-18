0be6daa79a1cbb9dd73654ad3e074e80d5aa4bd7
exoplanet <kalvitv@yahoo.com>
2021-09-18 15:42:00 +0530

    Use babel before minifying js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 
 const { src, dest } = require('gulp');
+var babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 
 exports.default = function() {
   return src('assets/js/*.js')
+    .pipe(babel())
     .pipe(uglify())
     .pipe(concat('kalvi-app.js'))    
     .pipe(dest('assets/js/'));
