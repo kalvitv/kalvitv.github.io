53ddffc986f7a6b8f8497763c0a34ab21454ce8b
exoplanet <kalvitv@yahoo.com>
2021-09-18 15:27:45 +0530

    Add support for local bundle and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
+
 const { src, dest } = require('gulp');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 exports.default = function() {
-  return src('assets/js/*.js')    
-    .pipe(concat('kalvi-app.js'))
+  return src('assets/js/*.js')
     .pipe(uglify())
+    .pipe(concat('kalvi-app.js'))    
     .pipe(dest('assets/js/'));
 }
