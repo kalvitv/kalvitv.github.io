bcd145cb9558578b2cf41ea0acd7149f097169ea
exoplanet <kalvitv@yahoo.com>
2021-09-18 23:41:33 +0530

    Add capability to play YT video

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,33 @@ $(function(){
         window.scrollTo(0,0);
     });
 });
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function showVideo(videoId){
+   $("#videomodal .modal-body").empty();
+   $("#videomodal .modal-body").append("<div id=\"player\"></div>");
+   let player = new YT.Player('player', {
+      height: '350',
+      width: '100%',
+      videoId: videoId,
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        'onReady': function (event) { event.target.playVideo(); }
+      }
+   });
+   
+   var myModal = new bootstrap.Modal(document.getElementById('videomodal'));
+   myModal.show();
+   
+   myModal.addEventListener('hidden.bs.modal', function (event) {
+      player.stopVideo();
+   });
+}
